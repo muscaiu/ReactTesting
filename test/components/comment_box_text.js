9b6afbe0348ca46a811dfb8265aaf3f74d7988b7
Cris <muscaiu@yahoo.com>
2017-09-12 07:08:03 +0300

    adding basic test file for comment_box component

---
+++ test/components/comment_box_text.js
@@ -0,0 +1,14 @@
+import { renderComponent, expect } from '../test_helper'
+import CommentBox from '../../src/components/comment_box'
+
+describe('CommentBox Component', () => {
+
+    it('has a text area', () => {
+
+    })
+
+    it('has a button', () => {
+
+    })
+
+})
