14cb389ee419435986110f8d70a5b4bd978a1054
Cris <muscaiu@yahoo.com>
2017-09-14 08:52:07 +0300

    testing add new text to textarea

--- test/components/comment_box_text.js
+++ test/components/comment_box_text.js
@@ -3,7 +3,6 @@ import CommentBox from '../../src/components/comment_box'
 
 describe('CommentBox', () => {
     let component
-
     beforeEach(() => {
         component = renderComponent(CommentBox)
     })
@@ -18,5 +17,20 @@ describe('CommentBox', () => {
 
     it('has a button', () => {
         expect(component.find('button')).to.exist
+    })
+
+    describe('entering some text', () => {
+        beforeEach(() => {
+            //simulate a change event to ad a new comment text
+            component.find('textarea').simulate('change', 'new comment')
+        })
+
+        it('shows that text in the textarea', () => {
+            expect(component.find('textarea')).to.have.value('new comment')
+        })
+
+        it('when submitted, clears the input', () => {
+
+        })
     })
 })
