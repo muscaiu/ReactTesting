9fa4f6ab9f283787b0e03629233def45c202f58f
Cris <muscaiu@yahoo.com>
2017-09-14 08:33:30 +0300

    expect App to contain Component

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,18 +1,15 @@
-import  {renderComponent, expect} from '../test_helper'
-import  App from '../../src/components/app'
+import { renderComponent, expect } from '../test_helper'
+import App from '../../src/components/app'
 
 // Use 'describe' to group together similar tests
-describe('App Component', ()=>{
-  
-  // USe 'it' to test a single attribute of a target
-  it('shows the correct text', ()=>{
+describe('App', () => {
+  let component
 
-    //create an instance of App
-    const component = renderComponent(App)
-
-    //Use 'expect' to make an 'assertion' about a target
-    expect(component).to.contain('React simple starter') 
-
+  beforeEach(() => {
+    component = renderComponent(App)
   })
 
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist
+  })
 })
