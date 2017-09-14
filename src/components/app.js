2d9b864591f2729bb549634465be7cd049583482
muscaiu <muscaiu@yahoo.com>
2017-09-14 13:43:13 +0300

    added basic test using state and props

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import CommentBox from './comment_box'
+import CommentBox from './commentBox'
+import CommentList from './commentList'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <CommentBox />
+        <CommentList />
       </div>
     )
   }
--- test/components/commentBox.spec.js
+++ test/components/commentBox.spec.js
@@ -1,5 +1,5 @@
 import { renderComponent, expect } from '../test_helper'
-import CommentBox from '../../src/components/comment_box'
+import CommentBox from '../../src/components/commentBox'
 
 describe('CommentBox', () => {
     let component
